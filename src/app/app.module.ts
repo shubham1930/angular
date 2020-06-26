383ff342488b86409896d088fba3b0b7b496a813
Shubham Kumar <shubhu.1930@gmail.com>
2020-06-26 08:15:21 +0530

    angular router basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,32 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { MatListModule } from '@angular/material/list';
 
 import 'hammerjs';
+import { AppComponent } from './app.component';
+
 import { MenuComponent } from './menu/menu.component';
-import { MatListModule } from '@angular/material/list';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { DishService } from './services/dish.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     DishdetailComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {Dish} from '../shared/dish';
-
+import { Dish } from '../shared/dish';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
@@ -8,8 +7,7 @@ import {Dish} from '../shared/dish';
 })
 export class DishdetailComponent implements OnInit {
 
-  @Input()
-  dish = Dish;
+  @Input() dish: Dish;
   constructor() { }
 
   ngOnInit(): void {
